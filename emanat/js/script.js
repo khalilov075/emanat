e390d7385f58c5dbd6540f45bfad3c5db05b8d8d
khalilov075 <37265936+khalilov075@users.noreply.github.com>
2018-05-20 19:46:56 +0400

    Add files via upload

---
+++ emanat/js/script.js
@@ -0,0 +1,14 @@
+var img = document.createElement("img");
+img.className = "emanatLogo"
+
+img.src = "img/emanatLogo.png";
+
+document.body.appendChild(img)
+
+var img = document.createElement("img");
+img.className = "information"
+
+img.src = "img/information.png";
+
+document.body.appendChild(img)
+
